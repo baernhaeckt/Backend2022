1651db3661510ecec1396bf7253bd50ae1cbd672
Hofmann Oliver <oliver.hofmann@ti8m.ch>
2022-08-27 23:33:33 +0200

    feat: Add new ingredients to the seeder

--- MixMeal.Core/Models/IngredientTag.cs
+++ MixMeal.Core/Models/IngredientTag.cs
@@ -10,4 +10,10 @@ public class IngredientTag
 
     public const string Grains = "Getreide und Hülsenfrüchte";
     public const string Fruits = "Früchte";
+    public const string Raw = "Roh";
+    public const string Steamed = "gedämpft";
+    public const string SeedsAndNuts = "Kerne und Nüsse";
+    public const string ExtraSideDishes = "extra Beilagen";
+    public const string GreenSalat = "grüner Salat";
+    public const string Cubes = "Cubes";
 }
